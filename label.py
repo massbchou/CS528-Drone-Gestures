80eaf5ffcbca567ae03f1993ca9dcdb66b202be0
AustinHCopley <austinhcopley@gmail.com>
2024-04-20 18:09:25 -0400

    added script to create label files

---
+++ label.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def create_labels(path):
+    files = []
+    for file in os.listdir(path):
+        if file.endswith(".csv"):
+            files.append(file[:-4])
+    return files
+
+def main():
+    data_dir = "./data/"
+    data = sorted(create_labels(data_dir))
+    direction_num = {'up': 0, 'down': 1, 'left': 2, 'right': 3}
+    labels = {x: direction_num[x[:-3]] for x in data}
+    print(labels, "\n", len(labels))
+
+    df = pd.DataFrame.from_dict(labels, orient='index')
+    df.to_csv(data_dir + 'labels.csv')
+
+    df['filename'] = df.index
+
+    train, val = train_test_split(df, test_size=0.2, random_state=1)
+
+    train.to_csv('./data/train.csv', index=False)
+    val.to_csv('./data/val.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
