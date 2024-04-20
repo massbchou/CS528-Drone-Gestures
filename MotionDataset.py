3274c9e23ea763d78b34577a31c659a5fd4f3f0a
AustinHCopley <austinhcopley@gmail.com>
2024-04-20 18:07:09 -0400

    added LSTM training file and custom dataset

---
+++ MotionDataset.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset
+
+class MotionData(Dataset):
+    def __init__(self, label_file, data_dir, transform=None):
+        self.labels = pd.read_csv(label_file)
+        self.data_dir = data_dir
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        filename = self.labels.iloc[idx]['filename']
+        filename = os.path.join(self.data_dir, (filename + ".csv"))
+        df = pd.read_csv(filename)
+        data = df[['acce_x', 'acce_y', 'acce_z', 'gyro_x', 'gyro_y', 'gyro_z']].values.astype(np.float32)
+
+        if self.transform:
+            data = self.transform(data)
+
+        label = self.labels.iloc[idx]['0']
+
+        return data, label
